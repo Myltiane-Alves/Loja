8fc8313532ed6b3a69d3c38e5c9489ef7ed8e8ec
Myltiane Alves <myltiane.aux@gmail.com>
2021-12-19 14:13:25 -0300

    Adicionando a navegação

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,  View, LogBox } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Header from './Shared/Header';
 import ProductContainer from './Screens/Products/ProductContainer';
+
+// Navigatiors
+import Main from "./Navigators/Main";
 
 LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header />
-      <ProductContainer />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Header />
+        <Main />
+      </View>
+
+    </NavigationContainer>
   );
 }
 
